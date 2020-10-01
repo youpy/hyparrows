2eb378a4f6aef5e8141b7400807d700426a3c65d
youpy <youpy@buycheapviagraonlinenow.com>
2020-10-01 23:17:50 +0900

    memoizes the rendering result of `Arrow`

--- src/arrow.tsx
+++ src/arrow.tsx
@@ -1,81 +1,80 @@
 import { getBoxToBoxArrow } from 'perfect-arrows';
 import React from 'react';
+import { Rect } from './interfaces';
 
 interface Props {
-  linkInPage: [HTMLAnchorElement, Element];
+  p1: Rect;
+  p2: Rect;
 }
 
-export const Arrow = (props: Props) => {
-  const {
-    linkInPage: [a, el],
-  } = props;
-  const scrollX = window.scrollX;
-  const scrollY = window.scrollY;
-  const r1 = a.getBoundingClientRect();
-  const r2 = el.getBoundingClientRect();
-  const p1 = {
-    x: r1.x + scrollX,
-    y: r1.y + scrollY,
-    w: r1.width,
-    h: r1.height,
-  };
-  const p2 = {
-    x: r2.x + scrollX,
-    y: r2.y + scrollY,
-    w: r2.width,
-    h: r2.height,
-  };
-  const arrow = getBoxToBoxArrow(
-    p1.x,
-    p1.y,
-    p1.w,
-    p1.h,
-    p2.x,
-    p2.y,
-    p2.w,
-    p2.h,
-    {
-      bow: 0.1,
-      stretch: 0.1,
-      stretchMin: 40,
-      stretchMax: 420,
-      padStart: 0,
-      padEnd: 12,
-      flip: false,
-      straights: true,
-    }
-  );
-  const [sx, sy, cx, cy, ex, ey, ae] = arrow;
-  const endAngleAsDegrees = ae * (180 / Math.PI);
-  const maxX = Math.max(p1.x + p1.w, p2.x + p2.w);
-  const maxY = Math.max(p1.y + p1.h, p2.y + p2.h);
+export const Arrow = React.memo(
+  (props: Props) => {
+    console.log(1);
+    const { p1, p2 } = props;
+    const arrow = getBoxToBoxArrow(
+      p1.x,
+      p1.y,
+      p1.w,
+      p1.h,
+      p2.x,
+      p2.y,
+      p2.w,
+      p2.h,
+      {
+        bow: 0.1,
+        stretch: 0.1,
+        stretchMin: 40,
+        stretchMax: 420,
+        padStart: 0,
+        padEnd: 12,
+        flip: false,
+        straights: true,
+      }
+    );
+    const [sx, sy, cx, cy, ex, ey, ae] = arrow;
+    const endAngleAsDegrees = ae * (180 / Math.PI);
+    const maxX = Math.max(p1.x + p1.w, p2.x + p2.w);
+    const maxY = Math.max(p1.y + p1.h, p2.y + p2.h);
 
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: 0,
-      }}
-    >
-      <svg
-        viewBox={`0 0 ${maxX} ${maxY}`}
+    return (
+      <div
         style={{
-          width: maxX,
-          height: maxY,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: 0,
         }}
-        stroke="#000"
-        fill="#000"
-        strokeWidth={3}
       >
-        <circle cx={sx} cy={sy} r={3} />
-        <path d={`M${sx},${sy} Q${cx},${cy} ${ex},${ey}`} fill="none" />
-        <polygon
-          points="0,-4 8,0, 0,4"
-          transform={`translate(${ex},${ey}) rotate(${endAngleAsDegrees})`}
-        />
-      </svg>
-    </div>
-  );
-};
+        <svg
+          viewBox={`0 0 ${maxX} ${maxY}`}
+          style={{
+            width: maxX,
+            height: maxY,
+          }}
+          stroke="#000"
+          fill="#000"
+          strokeWidth={3}
+        >
+          <circle cx={sx} cy={sy} r={3} />
+          <path d={`M${sx},${sy} Q${cx},${cy} ${ex},${ey}`} fill="none" />
+          <polygon
+            points="0,-4 8,0, 0,4"
+            transform={`translate(${ex},${ey}) rotate(${endAngleAsDegrees})`}
+          />
+        </svg>
+      </div>
+    );
+  },
+  (a, b) => {
+    return (
+      a.p1.x === b.p1.x &&
+      a.p1.y === b.p1.y &&
+      a.p1.w === b.p1.w &&
+      a.p1.h === b.p1.h &&
+      a.p2.x === b.p2.x &&
+      a.p2.y === b.p2.y &&
+      a.p2.w === b.p2.w &&
+      a.p2.h === b.p2.h
+    );
+  }
+);
--- src/arrows.tsx
+++ src/arrows.tsx
@@ -1,15 +1,36 @@
 import { useState, useMemo, useEffect } from 'react';
-import { Arrow } from './arrow';
 import React from 'react';
-
-type LinksInPage = [HTMLAnchorElement, Element][];
+import { Arrow } from './arrow';
+import { Rect } from './interfaces';
 
 interface Props {
   root: Element;
 }
 
+interface BothEndsOfArrow {
+  from: Rect;
+  to: Rect;
+}
+
+const elementToRect = (
+  element: Element,
+  scrollX: number,
+  scrollY: number
+): Rect => {
+  const r = element.getBoundingClientRect();
+
+  return {
+    x: r.x + scrollX,
+    y: r.y + scrollY,
+    w: r.width,
+    h: r.height,
+  };
+};
+
 export const Arrows = ({ root }: Props) => {
-  const [linksInPage, setLinksInPage] = useState<LinksInPage>([]);
+  const [bothEndsOfArrows, setBothEndsOfArrows] = useState<BothEndsOfArrow[]>(
+    []
+  );
   const [rev, setRev] = useState<number>(0);
 
   useEffect(() => {
@@ -37,7 +58,9 @@ export const Arrows = ({ root }: Props) => {
 
   useMemo(() => {
     const links = root.querySelectorAll('a');
-    const lip: LinksInPage = [];
+    const scrollX = window.scrollX;
+    const scrollY = window.scrollY;
+    const items: BothEndsOfArrow[] = [];
 
     links.forEach(
       (link) => {
@@ -48,20 +71,23 @@ export const Arrows = ({ root }: Props) => {
           const destination = root.querySelector(`[id="${id}"]`);
 
           if (destination) {
-            lip.push([link, destination]);
+            items.push({
+              from: elementToRect(link, scrollX, scrollY),
+              to: elementToRect(destination, scrollX, scrollY),
+            });
           }
         }
       },
       [rev]
     );
 
-    setLinksInPage(lip);
+    setBothEndsOfArrows(items);
   }, [rev]);
 
   return (
     <>
-      {linksInPage.map((l) => (
-        <Arrow linkInPage={l} />
+      {bothEndsOfArrows.map(({ from, to }) => (
+        <Arrow p1={from} p2={to} />
       ))}
     </>
   );
